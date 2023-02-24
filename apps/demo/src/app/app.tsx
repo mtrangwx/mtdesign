1c062ac94e1813bfe5a5e16222bc560ae174cdb2
Michael Trang <michael.trang@hotmail.com>
2023-02-24 18:22:24 +1100

    use DS core button

--- apps/demo/src/app/app.tsx
+++ apps/demo/src/app/app.tsx
@@ -1,11 +1,32 @@
-import styles from './app.module.css';
+// import styles from './app.module.css';
+// import './app.scss';
+
 import { useViewportSize } from '@mtdesign/use-viewport-size';
+import { isOdd }  from 'luv-is-odd';
+import { useState } from 'react';
+
+// import Button from '@woolworthslimited/core-react-components-button';
 
 export function App() {
   const size = useViewportSize();
+  const [ num, setNum] = useState<number>(1);
+  const handleUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNum(+e.target.value);
+  }
   return (
-    <div className={styles.app} data-testid="app">
-      {size.width}px / {size.height}px
+    <div data-testid="app">
+    {/* <div className={styles.app} data-testid="app"> */}
+
+      <button className="button primary">Button primary</button>
+      <button className="button secondary">Button secondary</button>
+      <button className="button destructive">Button destructive</button>
+
+      {/* <Button variant="secondary" size="small" onClick={() => console.log('clicked')}>
+        Save details
+      </Button> */}
+      {/* <div><input onChange={handleUpdate} value={num} /></div>
+      <div>{isOdd(num) === true ? 'odd' : 'even'}</div> */}
+      {/* {size.width}px / {size.height}px */}
     </div>
   );
 }
